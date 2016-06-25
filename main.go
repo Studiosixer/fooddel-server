877a1a32d2ccf9732e9051121e2def46e563e904
Wesley Eledui <wefilmer@gmail.com>
2016-06-25 11:46:42 -0500

    Fix port error

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"fmt"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":80", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
